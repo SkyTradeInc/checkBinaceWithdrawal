2651f5553bb3e3b05ca1498d74ffc002ab1c8a1e
Steve <admin@cryptostat.org>
2019-05-15 12:56:25 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const chalk = require('chalk')
+const beep = require('beepbeep')
+const axios = require('axios')
+
+const asset = "RVN"
+function walletStatus(asset) {
+      axios.get('https://www.binance.com/assetWithdraw/getAllAsset.html')
+        .then(response => {
+          const coins = response.data
+          for(let i=0; i<coins.length;i++) {
+            if(coins[i].assetCode == asset) {
+              if(coins[i].enableWithdraw) {
+                return console.log(chalk.green("ENABLED"))
+                beep()
+              }
+            }
+          }
+          return console.log(chalk.red("NOT ENABLED"))
+        })
+        .catch(console.log)
+    }
+
+walletStatus(asset)
+setInterval(() => {
+  walletStatus(asset)
+}, 30000)
